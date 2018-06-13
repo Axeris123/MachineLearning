97c4beb48949c875ee4a2a1f9763ac2530325e1c
artkwi <artkwi@st.amu.edu.pl>
2018-06-13 18:01:48 +0200

    Grap search

--- loadSerializedProgram.py
+++ loadSerializedProgram.py
@@ -6,6 +6,50 @@ with open('program_obj.pickl', 'rb') as f:
     obj = pickle.load(f)
 
 
+def find_path(graph, start, end, pathCost, path=[]):
+        path = path + [start]     
+        pathCost += timetable[start][2]
+        print(pathCost)
+        if start == end:
+            return path
+        if start not in graph:
+            return None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_path(graph, node, end, pathCost, path)
+                if newpath: 
+                    
+                    
+                    return newpath
+        return None
+
+
+def find_all_paths(graph, start, end, pathCost, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if start not in graph:
+            return []
+        paths = []
+        for node in graph[start]:
+            if node not in path:
+                newpaths = find_all_paths(graph, node, end, pathCost, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+
+
+def evaluate_paths(paths):
+    bestPath = []
+    bestMark = 0
+    for path in paths:
+        mark = 0
+        for node in path:
+            mark += timetable[node][2]
+        if (bestMark < mark):
+            bestMark = mark
+            bestPath = path 
+    return bestPath
 
 
 # normalize time
@@ -14,19 +58,16 @@ for key, value in obj.items():
     if len(value[0]) == 7:
         oldValue[0] = '0' + oldValue[0]
         obj[key] = oldValue
-#pprint(obj)
+pprint(obj)
 
 timetable = {}
 for key, value in obj.items():
     oldValue = value
     tempHour = int(value[0][0:2])
     if (value[0][6:8] == 'pm'):
-        tempHour = int(value[0][0:2]) + 12       
-        #print(int(oldValue[0][0:2]) + 12)
-    #print(tempHour)
+        tempHour = int(value[0][0:2]) + 12
     newValue = [tempHour, value[1], value[2]]
     timetable[key] = newValue
-pprint(timetable)
     
 
 # create graph
@@ -37,63 +78,15 @@ for key, value in timetable.items():
         if (value[0] + 2 < value2[0] and value[0] + 5 > value2[0]):
             nodeList.append(key2)
     graph[key] = nodeList
-pprint(graph)
 
 
-'''
-tempKey = ""
-tempValue = []
-tempMark = 0
+pathCost = 0
 
-scheduleAM = {}
-schedulePM = {}
-schedule = {}
+allPaths = find_all_paths(graph, 'The Big Trees', 'Return of the Seven', pathCost)
 
-# normalize time
-for key, value in obj.items():
-    oldValue = value
-    if len(value[0]) == 7:
-        oldValue[0] = '0' + oldValue[0]
-        obj[key] = oldValue
+bestPath = evaluate_paths(allPaths)
 
-# divide noon
-for key, value in obj.items():
-    if 'am' in value[0]:
-        scheduleAM[key] =  value
-    if 'pm' in value[0]:
-        schedulePM[key] =  value
+print("\n\n\n\n")
 
-# AM schedule
-hour = 0
-while (hour <= 10):
-    for key, value in scheduleAM.items():
-        if value[2] > tempMark and int(value[0][0:2]) > hour and int(value[0][0:2]) < hour+2:
-            tempKey = key
-            tempValue = value
-            tempMark = value[2]
-    if tempKey != "":            
-        schedule[tempKey] = tempValue
-    tempKey = ""
-    tempValue = []
-    tempMark = 0
-    hour+=2
-    
-# PM schedule
-hour = 0
-while (hour <= 10):
-    for key, value in schedulePM.items():
-        if value[2] > tempMark and int(value[0][0:2]) > hour and int(value[0][0:2]) < hour+2:
-            tempKey = key
-            tempValue = value
-            tempMark = value[2]
-    if tempKey != "":
-        schedule[tempKey] = tempValue
-    tempKey = ""
-    tempValue = []
-    tempMark = 0
-    hour+=2
-
-# show schedule
-for key, value in schedule.items():
-    print(key,"         ", value)
-'''
+for film in bestPath:
+    print(film,"     ", obj[film])
